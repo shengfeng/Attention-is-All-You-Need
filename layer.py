ca758a9a1e94e30f809ba514fdde5f8494e10184
shengfeng <shengfeng@ztgame.com>
2018-10-31 18:40:35 +0800

    实现注意力机制

---
+++ layer.py
@@ -0,0 +1,145 @@
+
+import mxnet as mx
+from mxnet import nd, gluon
+from mxnet.gluon import nn, rnn
+
+
+
+class Attention(nn.HybridBlock):
+    def __init__(self, Q_shape, K_shape, V_shape, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+        with self.name_scope():
+            self.Wq = self.params.get('Wq', shape=Q_shape)
+            self.Wk = self.params.get('Wk', shape=Q_shape)
+            self.Wv = self.params.get('Wv', shape=Q_shape)
+            self.qq = Q_shape[1]
+        self.Q_shape = Q_shape
+        self.K_shape = K_shape
+        self.V_shape = V_shape
+
+
+    def ScaledDotProductAttention(self, F, q, k, v):
+        S = F.batch_dot(q, F.transpose(k, axes=(0, 2, 1))) / (self.qq ** 0.5)
+        return F.batch_dot(F.softmax(S), v)
+
+
+    def hybrid_forward(self, F, q, k, v, Wq, Wk, Wv):
+        return self.ScaledDotProductAttention(F, F.dot(q, Wq), F.dot(k, Wk), F.dot(v, Wv))
+
+
+    def __repr__(self):
+        s = '{name}(Q_shape={Q_shape}, K_shape={K_shape}, V_shape={V_shape}, out_shape={out_shape})'
+        return s.format(
+            name = self.__class__.__name__,
+            Q_shape = str((self.Q_shape[1], self.Q_shape[0])),
+            K_shape = str((self.K_shape[1], self.K_shape[0])),
+            V_shape = str((self.V_shape[1], self.V_shape[0])),
+            out_shape = str((self.K_shape[1], self.V_shape[0])))
+
+
+
+class Multi_Head_Attention(nn.HybridBlock):
+    def __init__(self, Q_shape, K_shape, V_shape, h, **kwargs):
+        super(Multi_Head_Attention, self).__init__(**kwargs)
+        with self.name_scope():
+            for _ in range(h):
+                self.register_child(
+                    Attention(
+                        Q_shape=Q_shape,
+                        K_shape=K_shape,
+                        V_shape=V_shape))
+            self.Wo = self.params.get('Wo', shape=(h * V_shape[1], V_shape[0]))
+        self.h = h
+
+    def hybrid_forward(self, F, q, k, v, Wo):
+        H = []
+        for block in self._children.values():
+            H.append(block(q, k, v))
+        return F.dot(F.concat(*H, dim=2), Wo)
+
+    def __repr__(self):
+        s = '{name}({Attention}, h_num={h})'
+        return s.format(
+            name=self.__class__.__name__,
+            Attention=list(self._children.values())[0],
+            h=self.h)
+
+
+class Position_Embedding(nn.HybridBlock):
+    def __init__(self, shape, **kwargs):
+        super(Position_Embedding, self).__init__(**kwargs)
+        self.shape = shape
+
+    def hybrid_forward(self, F, x):
+        seq_len = self.shape[0]
+        position_size = self.shape[1]
+
+        position_j = 1. / F.broadcast_power(
+            10000 * F.ones(shape=(1,1)),
+            (2 * F.arange(position_size / 2) / position_size))
+
+        position_i = F.arange(seq_len, dtype='float32')
+        position_i = F.expand_dims(position_i, 1)
+        position_ij = F.dot(position_i, position_j)
+        position_ij = F.concat(
+            *[F.cos(position_ij), F.sin(position_ij)], dim=1)
+        position_ij = F.broadcast_add(
+            F.expand_dims(position_ij, 0), F.zeros_like(x))
+
+        return position_ij + x
+
+
+
+
+class SANet(gluon.HybridBlock):
+    def __init__(self, shape, Vocab_len, h, is_PE=True, **kwargs):
+        super(SANet, self).__init__(**kwargs)
+        self.embed = nn.Embedding(input_dim=Vocab_len, output_dim=shape[0])
+        self.PE = Position_Embedding(shape=(shape[1], shape[0]))
+        self.MHA = Multi_Head_Attention(
+            Q_shape=shape, K_shape=shape, V_shape=shape, h=h)
+        self.liner = gluon.nn.Dense(2)
+        self.pool = gluon.nn.GlobalAvgPool1D()
+        self.droup = gluon.nn.Dropout(.5)
+        self.shape = (shape[1], shape[0])
+        self.Is_PE = is_PE
+
+
+    def hybrid_forward(self, F, x):
+        # kqv = self.PE(self.embed(x))
+        kqv = self.embed(x)
+        if self.Is_PE:
+            kqv = self.PE(kqv)
+
+        return self.liner(self.droup(self.pool(self.MHA(kqv, kqv, kqv))))
+
+
+
+class LSTMANet(gluon.HybridBlock):
+    def __init__(self, shape, Vocab_len, h,  num_hiddens, embed_size, num_layers, is_PE, **kwargs):
+        super(LSTMANet, self).__init__(**kwargs)
+        self.embed = nn.Embedding(input_dim=Vocab_len, output_dim=shape[0])
+        self.PE = Position_Embedding(shape=(shape[1], shape[0]))
+        self.MHA = Multi_Head_Attention(
+            Q_shape=shape, K_shape=shape, V_shape=shape, h=h)
+        self.liner = gluon.nn.Dense(2)
+        self.pool = gluon.nn.GlobalAvgPool1D()
+        self.drop = gluon.nn.Dropout(.5)
+        self.shape = (shape[1], shape[0])
+        self.Is_PE = is_PE
+        self.lstm = rnn.LSTM(num_hiddens, num_layers=num_layers,
+                             bidirectional=True, input_size=embed_size)
+
+
+    def hybrid_forward(self, F, x):
+        # kqv = self.PE(self.embed(x))
+        kqv = self.embed(x)
+        if self.Is_PE:
+            kqv = self.PE(kqv)
+
+        output = self.lstm(kqv)
+        output = self.MHA(output, output, output)
+        output = self.pool(output)
+        output = self.drop(output)
+        output = self.liner(output)
+        return output
---
+++ train.py
@@ -0,0 +1,189 @@
+
+from collections import Counter
+import os
+import random
+import time
+import mxnet as mx
+from mxnet import autograd, gluon, init, nd
+from mxnet.contrib import text
+from tqdm import tqdm
+from layer import *
+
+
+def readIMDB(dir_url, seg='train'):
+    pos_or_neg = ['pos', 'neg']
+    dataset = []
+    for lb in pos_or_neg:
+        files = os.listdir(dir_url + '/' + seg + '/' + lb + '/')
+        for file in tqdm(files):
+            with open(
+                dir_url + '/' + seg + '/' + lb + '/' + file, 'r', encoding='utf8') as rf:
+                review = rf.read().replace('\n', '')
+                if lb == 'pos':
+                    dataset.append([review, 1])
+                elif lb == 'neg':
+                    dataset.append([review, 0])
+    return dataset
+
+
+data_dir = 'data/'
+context = mx.gpu()
+batch_size = 32
+max_len = 100
+embedding_size = 128
+kernel_sizes  = [3, 4, 5]
+nums_channels = [100, 100, 100]
+
+
+train_dataset = readIMDB(data_dir, 'train')
+test_dataset = readIMDB(data_dir, 'test')
+
+
+random.shuffle(train_dataset)
+random.shuffle(test_dataset)
+
+
+def tokenizer(text):
+    return [tok.lower() for tok in text.split(' ')]
+
+
+
+train_tokenized = []
+train_labels = []
+for review, score in train_dataset:
+    train_tokenized.append(tokenizer(review))
+    train_labels.append(score)
+test_tokenized = []
+test_labels = []
+for review, score in test_dataset:
+    test_tokenized.append(tokenizer(review))
+    test_labels.append(score)
+
+
+
+token_counter = Counter()
+
+def count_token(train_tokenized):
+    for sample in train_tokenized:
+        for token in sample:
+            if token not in token_counter:
+                token_counter[token] = 1
+            else:
+                token_counter[token] += 1
+
+
+count_token(train_tokenized)
+vocab = text.vocab.Vocabulary(token_counter, unknown_token='<unk>', reserved_tokens=None)
+
+
+
+# 根据词典，将数据转换成特征向量。
+def encode_samples(x_raw_samples, vocab):
+    x_encoded_samples = []
+    for sample in x_raw_samples:
+        x_encoded_sample = []
+        for token in sample:
+            if token in vocab.token_to_idx:
+                x_encoded_sample.append(vocab.token_to_idx[token])
+            else:
+                x_encoded_sample.append(0)
+        x_encoded_samples.append(x_encoded_sample)
+    return x_encoded_samples
+
+
+def pad_samples(x_encoded_samples, maxlen=100, val=0):
+    x_samples = []
+    for sample in x_encoded_samples:
+        if len(sample) > maxlen:
+            new_sample = sample[:maxlen]
+        else:
+            num_padding = maxlen - len(sample)
+            new_sample = sample
+            for i in range(num_padding):
+                new_sample.append(val)
+        x_samples.append(new_sample)
+    return x_samples
+
+
+
+x_encoded_train = encode_samples(train_tokenized, vocab)
+x_encoded_test = encode_samples(test_tokenized, vocab)
+
+x_train = nd.array(pad_samples(x_encoded_train, max_len, 0), ctx=context)
+x_test = nd.array(pad_samples(x_encoded_test, max_len, 0), ctx=context)
+
+y_train = nd.array([score for text, score in train_dataset], ctx=context)
+y_test = nd.array([score for text, score in test_dataset], ctx=context)
+
+
+
+
+train_data = gluon.data.ArrayDataset(x_train, y_train)
+train_dataloader = gluon.data.DataLoader(
+    train_data, batch_size=batch_size, shuffle=True)
+
+test_data = gluon.data.ArrayDataset(x_test, y_test)
+test_dataloader = gluon.data.DataLoader(
+    test_data, batch_size=batch_size, shuffle=True)
+
+
+def eval(dataloader):
+    total_L = 0
+    ntotal = 0
+    accuracy = mx.metric.Accuracy()
+    for data, label in dataloader:
+        output = net(data)
+        label = label.as_in_context(context)
+        L = loss(output, label)
+        total_L += nd.sum(L).asscalar()
+        ntotal += L.size
+        predicts = nd.argmax(output, axis=1)
+        accuracy.update(preds=predicts, labels=label)
+    return total_L / ntotal, accuracy.get()[1]
+
+
+
+net = SANet(shape=(embedding_size, max_len), Vocab_len=len(vocab), h=8, is_PE=True)
+net.initialize(mx.init.Uniform(.1), ctx=context)
+net.hybridize()
+trainer = gluon.Trainer(net.collect_params(), 'Adam')
+loss = gluon.loss.SoftmaxCrossEntropyLoss()
+loss.hybridize()
+
+
+num_epochs = 30
+start_train_time = time.time()
+for epoch in range(num_epochs):
+    start_epoch_time = time.time()
+    total_L = 0
+    ntotal = 0
+
+    for i, (data, label) in enumerate(train_dataloader):
+        with autograd.record():
+            output = net(data)
+            L = loss(output, label.as_in_context(context))
+        L.backward()
+        trainer.step(batch_size)
+        total_L += nd.sum(L).asscalar()
+        ntotal += L.size
+
+        if i % 30 == 0 and i != 0:
+            print('Epoch %d. batch %d. Loss %6f' % (epoch, i, total_L / ntotal))
+            total_L = 0
+            ntotal = 0
+
+    print('performing testing')
+    train_loss, train_acc = eval(train_dataloader)
+    test_loss, test_acc = eval(test_dataloader)
+
+    print('[epoch %d] train loss %.6f, train accuracy %.6f' % (epoch, train_loss, train_acc))
+    print('[epoch %d] test loss %.6f, test accuracy %.6f' % (epoch, test_loss, test_acc))
+
+    print('[epoch %d] throughput %.2f samples/s' %
+          (epoch, (batch_size * len(x_train)) / (time.time() - start_epoch_time)))
+    print('[epoch %d] total time %.2f s' % (epoch, (time.time() - start_epoch_time)))
+
+print('total training throughput %.2f samples/s' %
+      ((batch_size * len(x_train) * num_epochs) / (time.time() - start_train_time)))
+print('total training time %.2f s' % ((time.time() - start_train_time)))
+
